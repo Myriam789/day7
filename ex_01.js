145289db27a95f310e832b74c1415574f3be3cdf
Myriam789 <myriam.sall@epitech.digital>
2024-10-08 13:06:14 +0200

    ex_01.js

---
+++ ex_01.js
@@ -0,0 +1,10 @@
+function reduceOpacity() {
+   const currentOpacity = parseFloat(getComputedStyle(square).opacity);
+   square.style.opacity = currentOpacity * 0,5;
+                      }
+
+function resetOpacity() {
+    square.style.opacity = 1;
+}
+square.addEventListener('mouseover', reduceOpacity)
+square.addEventListener('mouseout', reduceOpacity)
